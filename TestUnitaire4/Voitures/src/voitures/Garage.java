f28699ccf0663bcbb41490cca205946c114c9c40
Erwan SEVELLEC <erwan.sevellec@imt-atlantique.net>
2018-09-12 22:45:18 +0200

    end TP4

--- TestUnitaire4/Voitures/src/voitures/Garage.java
+++ TestUnitaire4/Voitures/src/voitures/Garage.java
@@ -7,72 +7,80 @@ public class Garage {
 	private int tailleMaxGarage;
 	private int nombreAutos;
 
-	Garage(int taille_max_garage)
-	{
+	Garage(int taille_max_garage) {
 		this.The_garage = new Auto[taille_max_garage];
 		this.nombreAutos = 0;
 		this.tailleMaxGarage = taille_max_garage;
 	}
 
-	void ajouter(Auto a)
-	{
+	void ajouter(Auto a) {
 		if (this.nombreAutos < this.tailleMaxGarage) {
 			this.The_garage[this.nombreAutos] = a;
 			this.nombreAutos++;
-		}
-		else System.out.println("Garage d�j� satur�");
+		} else
+			System.out.println("Garage d�j� satur�");
 	}
 
-	void afficher()
-	{
+	void afficher() {
 		System.out.println("Affichage du garage");
 		System.out.println();
-		for(int i=0; i < this.nombreAutos; i++){
-			System.out.println("   Voiture num�ro "+i+" :");
+		for (int i = 0; i < this.nombreAutos; i++) {
+			System.out.println("   Voiture num�ro " + i + " :");
 			this.The_garage[i].afficher();
 			System.out.println();
-		}	
-	}
-	
-	//-----------------------------------------------------------------
-	
-	public int f1(String m)
-	{
-		return 0;
+		}
 	}
 
-	public int f2(String m)
-	{
+	// -----------------------------------------------------------------
+
+	public int f1(String m) {
 		int compteur = 0;
-		for (Auto auto : The_garage) {
-			if (auto.equals(m)) {
+		for (int i = 0; i < nombreAutos; i++) {
+			if (The_garage[i].getMarque().equals(m)) {
 				compteur++;
 			}
 		}
 		return compteur;
 	}
 
-	public int f3()
-	{
-		return 0;
-	}
-	
-	public boolean f4()
-	{
-		return The_garage.length == 0;
-	}
-
-	public int f5()
-	{
-		return 0;
+	public int f2(String m) {
+		int compteur = 0;
+		for (int i = 0; i < nombreAutos; i++) {
+			if (The_garage[i].getModele().equals(m)) {
+				compteur++;
+			}
+		}
+		return compteur;
 	}
 
-	public int f6()
-	{
+	public int f3() {
+		return nombreAutos;
+	}
+
+	public boolean f4() {
+		return nombreAutos == 0;
+	}
+
+	public int f5() {
+		if (nombreAutos == 0) {
+			return 0;
+		}
+		int indice = 0;
+		float consoMin = The_garage[0].getConso();
+		for (int i = 1; i < nombreAutos; i++) {
+			if (The_garage[i].getConso() < consoMin) {
+				indice = i;
+				consoMin = The_garage[i].getConso();
+			}
+		}
+		return indice;
+	}
+
+	public int f6() {
 		int indice = 0;
 		float prix = -1;
-		for (int i = 0; i < The_garage.length; i++) {
-			if(The_garage[i].getPrix() > prix) {
+		for (int i = 0; i < nombreAutos; i++) {
+			if (The_garage[i].getPrix() > prix) {
 				prix = The_garage[i].getPrix();
 				indice = i;
 			}
@@ -80,32 +88,58 @@ public class Garage {
 		return indice;
 	}
 
-	public float f7()
-	{
-		return 0;
-	}
-
-	public int f8()
-	{
-		if(The_garage.length == 0) {
+	public float f7() {
+		if (nombreAutos == 0) {
 			return 0;
 		}
-		
-		int somme_km = 0;
-		for (Auto auto : The_garage) {
-			somme_km += auto.getKilometrage();
+		float prixMax = The_garage[0].getPrix();
+		float prixMin = prixMax;
+		for (int i = 1; i < nombreAutos; i++) {
+			float prix = The_garage[i].getPrix();
+			if (prix > prixMax) {
+				prixMax = prix;
+			} else if (prix < prixMin) {
+				prixMin = prix;
+			}
 		}
-		return somme_km / The_garage.length;
+		return prixMax - prixMin;
 	}
 
-	public int f9()
-	{
-		return 0;
+	public int f8() {
+		if (nombreAutos == 0) {
+			return 0;
+		}
+
+		int somme_km = 0;
+		for (int i = 0; i < nombreAutos; i++) {
+			somme_km += The_garage[i].getKilometrage();
+		}
+		return somme_km / nombreAutos;
 	}
 
-	public int f10(int n, int k)
-	{
-		return 0;
+	public int f9() {
+		int indice = -1;
+		float prixMin = 999999999;
+		for (int i = 0; i < nombreAutos; i++) {
+			if(The_garage[i].getNbPlaces() >= 5 && The_garage[i].getNbPortes() == 5 && The_garage[i].getPrix() < prixMin) {
+				indice = i;
+				prixMin = The_garage[i].getPrix();
+			}
+		}
+		return indice;
+	}
+
+	public int f10(int n, int k) {
+		int indice = -1;
+		float prixMin = 999999999;
+		for (int i = 0; i < nombreAutos; i++) {
+			if (The_garage[i].getNbPortes() >= n && The_garage[i].getKilometrage() < k
+					&& The_garage[i].getPrix() < prixMin) {
+				indice = i;
+				prixMin = The_garage[i].getPrix();
+			}
+		}
+		return indice;
 	}
 
 }
--- TestUnitaire4/Voitures/test/voitures/GarageTest.java
+++ TestUnitaire4/Voitures/test/voitures/GarageTest.java
@@ -17,10 +17,10 @@ public class GarageTest {
 	private Auto auto1;
 	private Auto auto2;
 	private Auto auto3;
-	
+
 	@Rule
-	  public ErrorCollector collector= new ErrorCollector();
-	
+	public ErrorCollector collector = new ErrorCollector();
+
 	public GarageTest() {
 	}
 
@@ -38,7 +38,7 @@ public class GarageTest {
 	@After
 	public void tearDown() {
 	}
-	
+
 	/**
 	 * Test of f1 method, of class Garage.
 	 */
@@ -46,7 +46,7 @@ public class GarageTest {
 	public void testF1() {
 		collector.checkThat("Test F1", garage1.f1("Peugeot"), is(2));
 	}
-	
+
 	/**
 	 * Test of f2 method, of class Garage.
 	 */
@@ -54,7 +54,7 @@ public class GarageTest {
 	public void testF2() {
 		collector.checkThat("Test F2", garage1.f2("206"), is(1));
 	}
-	
+
 	/**
 	 * Test of f3 method, of class Garage.
 	 */
@@ -62,7 +62,7 @@ public class GarageTest {
 	public void testF3() {
 		collector.checkThat("Test F3", garage1.f3(), is(3));
 	}
-	
+
 	/**
 	 * Test of f4 method, of class Garage.
 	 */
@@ -70,7 +70,7 @@ public class GarageTest {
 	public void testF4() {
 		collector.checkThat("Test F4", garage1.f4(), is(false));
 	}
-	
+
 	/**
 	 * Test of f5 method, of class Garage.
 	 */
@@ -78,7 +78,7 @@ public class GarageTest {
 	public void testF5() {
 		collector.checkThat("Test F5", garage1.f5(), is(2));
 	}
-	
+
 	/**
 	 * Test of f6 method, of class Garage.
 	 */
@@ -86,15 +86,15 @@ public class GarageTest {
 	public void testF6() {
 		collector.checkThat("Test F6", garage1.f6(), is(2));
 	}
-	
+
 	/**
 	 * Test of f7 method, of class Garage.
 	 */
 	@Test
 	public void testF7() {
-		collector.checkThat("Test F7", garage1.f7(), is(42500));
+		collector.checkThat("Test F7", garage1.f7(), is((float) 42500));
 	}
-	
+
 	/**
 	 * Test of f8 method, of class Garage.
 	 */
@@ -102,21 +102,21 @@ public class GarageTest {
 	public void testF8() {
 		collector.checkThat("Test F8", garage1.f8(), is(80000));
 	}
-	
+
 	/**
 	 * Test of f9 method, of class Garage.
 	 */
 	@Test
 	public void testF9() {
-		collector.checkThat("Test F9", garage1.f9(), is(0));
+		collector.checkThat("Test F9", garage1.f9(), is(1));
 	}
-	
+
 	/**
 	 * Test of f10 method, of class Garage.
 	 */
 	@Test
 	public void testF10() {
-		collector.checkThat("Test F10", garage1.f10(5, 50000), is(0));
+		collector.checkThat("Test F10", garage1.f10(5, 50000), is(1));
 	}
 
 }
